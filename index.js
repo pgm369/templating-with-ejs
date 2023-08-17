8b3e4ee07c727a0c230c6e2bcbd5c424d1ba599c
Replit user <>
2023-08-17 11:28:21 +0000

    Adds pages

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express();
+const port = 3000;
+
+var data = require('./data/test.json');
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+  let title = "Home";
+  res.render('pages/index', { "title": title });
+});
+
+app.get('/about', (req, res) => {
+  let title = "About Page";
+  res.render('pages/about', { "title": title });
+});
+
+//added /projects page
+app.get('/projects', (req, res) => {
+  let title = "Projects Page";
+  res.render('pages/projects', { "title": title });
+});
+
+//added /contact page
+app.get('/contact', (req, res) => {
+  let title = "Contact Page";
+  res.render('pages/contact', { "title": title });
+});
+
+//users index is our list page
+app.get('/users', function(req, res) {
+  var title = 'Users Page';
+  res.render('users/index', {
+    title: title,
+    users: data
+  });
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+  var title = 'User Page';
+  var id = req.params.id;
+  res.render('users/view', {
+    title: title,
+    user: data[--id]
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  console.log(data);
+});
